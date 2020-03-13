9d86789697dc0c92bf53ed105b333f7286d33896
Lukas Bauer <lukas.bauer.j@gmail.com>
2020-03-13 15:57:04 -0400

    Deleted unnecessary files

--- streamStaffCode/sams_thread.py
+++
@@ -1,8 +0,0 @@
-import threading
-from testPylsl import running
-
-thread1 = threading.Thread(target= running)
-thread1.start()
-
-thread2 = threading.Thread(target= running)
-thread2.start()
--- streamStaffCode/threads.py
+++
@@ -1,9 +0,0 @@
-import threading
-
-
-def runFunc(functionToRun, argsToRun=None):
-    if argsToRun:
-        func = threading.Thread(target=functionToRun, kwargs=argsToRun)
-    else:
-        func = threading.Thread(target=functionToRun)
-    func.run()
